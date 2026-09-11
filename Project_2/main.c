411f0afd2cd76cd05d7b9f2084483298096cc8eb
VelaLosa <yonsei26sw@gmail.com>
2026-09-11 23:37:55 +0900

    Add main.c

---
+++ Project_2/main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main() {
+    int input_num = 0;
+
+    scanf("%d", &input_num);
+
+    printf("%d\n", input_num);
+
+    return 0;
+}
